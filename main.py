6499942429554564f7be7d04441949a621393f28
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-06-26 19:31:09 +0530

    Adding Food and velocity

--- main.py
+++ main.py
@@ -24,13 +24,15 @@ exit_game = False
 game_over = False
 snake_x = 400
 snake_y = 250
-snake_size = 15
+snake_size = 10
+initial_velocity = 5
 velocity_x = 0
 velocity_y = 0
-food_x = random.randint(0, screen_width)
-food_y = random.randint(0, screen_height)
+food_x = random.randint(int(screen_width / 6), int(5 * screen_width / 6))
+food_y = random.randint(int(screen_height / 6), int(5 * screen_height / 6))
 food_size = 5.0
 fps = 60
+score = 0
 
 clock = pygame.time.Clock()
 # Game Loop
@@ -41,26 +43,32 @@ while not exit_game:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                velocity_x = 10
+                velocity_x = initial_velocity
                 velocity_y = 0
 
             if event.key == pygame.K_LEFT:
-                velocity_x = -10
+                velocity_x = -initial_velocity
                 velocity_y = 0
 
             if event.key == pygame.K_UP:
                 velocity_x = 0
-                velocity_y = -10
+                velocity_y = -initial_velocity
 
             if event.key == pygame.K_DOWN:
                 velocity_x = 0
-                velocity_y = 10
+                velocity_y = initial_velocity
 
     snake_x += velocity_x
     snake_y += velocity_y
 
+    if abs(snake_x - food_x) < 10 and abs(snake_y - food_y) < 10:
+        score += 1
+        print("Score:", score)
+        food_x = random.randint(int(screen_width / 6), int(5 * screen_width / 6))
+        food_y = random.randint(int(screen_height / 6), int(5 * screen_height / 6))
+
     gameWindow.fill(white)
-    pygame.draw.circle(gameWindow, red,(food_x,food_y),food_size)
+    pygame.draw.circle(gameWindow, red, (food_x, food_y), food_size)
     pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
     clock.tick(fps)
