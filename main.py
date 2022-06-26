06b5f7556c88a4058a2b03f339ba089328e6d066
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-06-26 18:05:12 +0530

    Adding Food and velocity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -23,17 +24,46 @@ exit_game = False
 game_over = False
 snake_x = 400
 snake_y = 250
-snake_size = 10
+snake_size = 15
+velocity_x = 0
+velocity_y = 0
+food_x = random.randint(0, screen_width)
+food_y = random.randint(0, screen_height)
+food_size = 5.0
+fps = 60
 
+clock = pygame.time.Clock()
 # Game Loop
 while not exit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = True
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                velocity_x = 10
+                velocity_y = 0
+
+            if event.key == pygame.K_LEFT:
+                velocity_x = -10
+                velocity_y = 0
+
+            if event.key == pygame.K_UP:
+                velocity_x = 0
+                velocity_y = -10
+
+            if event.key == pygame.K_DOWN:
+                velocity_x = 0
+                velocity_y = 10
+
+    snake_x += velocity_x
+    snake_y += velocity_y
+
     gameWindow.fill(white)
+    pygame.draw.circle(gameWindow, red,(food_x,food_y),food_size)
     pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
+    clock.tick(fps)
 
 pygame.quit()
 quit()
